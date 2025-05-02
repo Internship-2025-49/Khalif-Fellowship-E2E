340874f89331cada5b17efc894287cac2871058d
Khahaha10 <knurfattah@gmail.com>
2025-05-02 11:36:18 +0700

    feat: add playwright logout.ts model

---
+++ tests/models/logout.ts
@@ -0,0 +1,24 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class PlaywrightLogout {
+  readonly page: Page;
+  readonly dropdownLogout: Locator;
+  readonly buttonLogout: Locator;
+  readonly checkLogout: Locator;
+
+  constructor(page: Page) {
+    this.dropdownLogout = page.getByRole("button", {
+      name: "Khalif - int Profile Picture",
+    });
+    this.buttonLogout = page.getByRole("menuitem", { name: "Signout" });
+    this.checkLogout = page.getByRole("heading", {
+      name: "Sign in to your account",
+    });
+  }
+
+  async logout() {
+    await this.dropdownLogout.click();
+    await this.buttonLogout.click();
+    await expect(this.checkLogout).toBeVisible();
+  }
+}
