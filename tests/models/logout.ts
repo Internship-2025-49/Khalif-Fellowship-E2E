6a4928c5ad87c7bdcc3c9752fa8a697c445d7dac
Khahaha10 <knurfattah@gmail.com>
2025-05-06 11:43:35 +0700

    fix: remove checkLogout locator and related visibility expectation in logout method

--- tests/models/logout.ts
+++ tests/models/logout.ts
@@ -4,21 +4,16 @@ export class PlaywrightLogout {
   readonly page: Page;
   readonly dropdownLogout: Locator;
   readonly buttonLogout: Locator;
-  readonly checkLogout: Locator;
 
   constructor(page: Page) {
     this.dropdownLogout = page.getByRole("button", {
       name: /.*Profile Picture$/,
     });
     this.buttonLogout = page.getByRole("menuitem", { name: "Signout" });
-    this.checkLogout = page.getByRole("heading", {
-      name: "Sign in to your account",
-    });
   }
 
   async logout() {
     await this.dropdownLogout.click();
     await this.buttonLogout.click();
-    await expect(this.checkLogout).toBeVisible();
   }
 }
